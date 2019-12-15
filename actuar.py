cfd45c88ce8d170e49f411d31ebfcadd3e4cf17a
rarmas <rarmas@gmail.com>
2019-12-14 22:06:36 -0300

    Create actuar.py

---
+++ actuar.py
@@ -0,0 +1,36 @@
+toggle descriptive_variables replace spaces with _
+
+tener la capacidad de analizar una "situación" y comportarse acorde, y aceptar
+modificaciones de contexto y escribir la "situación"
+
+"""
+el sistema ha sido hackeado por una forma de humanidad saqueadora y ventajista
+ hay que volver a permitir el cambio de reglas del sistema
+
+el sistema hackeado ha intentado esconder su implementación de muchas formas,
+porque en esta era de los algoritmos habría demasiados humanos que entendieran
+su significado y pudieran parchearlo.
+"""
+
+class sentido_de_mejora_de_especie()
+        pass
+
+class humano(animal):
+        sentido_de_supervivencia_de_especie = True
+
+
+
+situacion = nuestra vision de la realidad
+
+if teatro:
+        situación = nuestra_visión_de_la_realidad_planteada_en_la_obra.
+
+
+class realidad()
+        self._analizada = False
+
+def analizar(realidad):
+        return realidad
+
+def act(reality):
+        return comportamiento
